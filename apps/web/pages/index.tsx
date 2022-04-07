04516da9f1f999c655486d33ad8810d9ac10e8c5
Jimmy DC <jimmydalecleveland@gmail.com>
2022-04-06 19:18:51 -0600

    add minimal microbundle setup with React

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Button } from "ui";
+import { Button as MicrobundleButton } from "microbundle-ui";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
       <Button />
+      <MicrobundleButton>Microbundle Button!</MicrobundleButton>
     </div>
   );
 }
