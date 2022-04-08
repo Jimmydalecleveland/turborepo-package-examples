d9f8a92f4e57923747405424f02126e8eea794dc
Jimmy DC <jimmydalecleveland@gmail.com>
2022-04-07 21:16:48 -0600

    add jest-dom and basic test for button

--- packages/esbuild-ui/src/Button/Button.test.tsx
+++ packages/esbuild-ui/src/Button/Button.test.tsx
@@ -1,12 +1,22 @@
 import * as React from "react";
 import { Button } from ".";
-import { defaultTheme } from "../styles.css";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 test("Button renders", () => {
-  render(
-    <div className={defaultTheme}>
-      <Button>Click Me!</Button>
-    </div>
-  );
+  const { container } = render(<Button>Click Me!</Button>);
+
+  expect(screen.getByRole("button")).toHaveTextContent(/click me/i);
+
+  // I imagine this won't work because anytime we change any styles
+  // the hash on each class will change and we'll have to update
+  // the snapshot without knowing what we are updating.
+  expect(container.firstChild).toMatchInlineSnapshot(`
+    <button
+      class="styles_pWrapper__pzbt8v8 styles_variant_primary__pzbt8v6"
+      type="button"
+    >
+      Click Me!
+    </button>
+  `);
 });
