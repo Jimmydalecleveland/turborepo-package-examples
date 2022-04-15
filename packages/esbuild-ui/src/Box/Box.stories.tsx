f158d2ea021590132daed9aebc477cf1a52026fd
Jimmy DC <jimmydalecleveland@gmail.com>
2022-04-14 19:16:13 -0600

    add basic Box stories

---
+++ packages/esbuild-ui/src/Box/Box.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Box from "./Box";
+
+export default {
+  title: "Components/Box",
+  component: Box,
+} as ComponentMeta<typeof Box>;
+
+const Template: ComponentStory<typeof Box> = (args) => <Box {...args} />;
+
+export const Minimal = Template.bind({});
+Minimal.args = {
+  padding: "medium",
+  children: "A Box Container",
+};
+
+export const ChooseComponent = Template.bind({});
+ChooseComponent.args = {
+  Component: "span",
+  children: "This Box wrapper is a span",
+};
