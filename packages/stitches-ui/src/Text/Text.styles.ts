ed19eadd689cff157a4ae140fa386d19d9ee4450
Jimmy DC <jimmydalecleveland@gmail.com>
2022-04-19 12:28:05 -0600

    add Text component, convert from VE to Stitches

---
+++ packages/stitches-ui/src/Text/Text.styles.ts
@@ -0,0 +1,31 @@
+import { css } from "../../stitches.config";
+
+export const text = css({
+  margin: 0,
+  padding: 0,
+
+  variants: {
+    size: {
+      small: {
+        fontSize: "$small",
+      },
+      medium: {
+        fontSize: "$medium",
+      },
+      large: {
+        fontSize: "$large",
+      },
+    },
+    weight: {
+      thin: {
+        fontWeight: 300,
+      },
+      regular: {
+        fontWeight: 400,
+      },
+      strong: {
+        fontWeight: 600,
+      },
+    },
+  },
+});
